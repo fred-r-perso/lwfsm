381079be14869684579939a77a5f35470e52492f
fred-r <fredr2010@gmail.com>
2023-02-19 20:18:06 +0100

    fix: CI build script - openssl

--- ci/build_linux_example.sh
+++ ci/build_linux_example.sh
@@ -17,7 +17,7 @@ set -x
 ###################
 
 apt-get update
-apt-get -y install git rsync gpg ca-certificates wget software-properties-common tar make build-essential
+apt-get -y install git rsync gpg ca-certificates wget software-properties-common tar make build-essential openssl
 
 # latest CMAKE
 #apt-get -y update && \
