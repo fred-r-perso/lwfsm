d11f4fe070386c10155374b9fc009efe37437fbe
fred-r <fredr2010@gmail.com>
2023-02-19 09:47:01 +0100

    fix: ci build issue

--- ci/build_linux_example.sh
+++ ci/build_linux_example.sh
@@ -17,34 +17,26 @@ set -x
 ###################
 
 apt-get update
-apt-get -y install git rsync
+apt-get -y install git rsync gpg
 
 # latest CMAKE
-sudo apt update && \
-sudo apt install -y software-properties-common lsb-release && \
-sudo apt clean all
+apt-get -y update && \
+apt-get -y install -y software-properties-common lsb-release && \
+apt-get -y clean all
 
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 
-sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+apt-get -y-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
 
-sudo apt update
-sudo apt install kitware-archive-keyring
-sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
+apt-get -y update
+apt-get -y install kitware-archive-keyring
+rm /etc/apt/trusted.gpg.d/kitware.gpg
 
-sudo apt update
-sudo apt install cmake
+apt-get -y update
+apt-get -y install cmake
 
 # Ownership issue patch
 git config --global --add safe.directory /__w/lwfsm/lwfsm
-
-#####################
-# DECLARE VARIABLES #
-#####################
-
-pwd
-ls -lah
-export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 
 #################
 # BUILD EXAMPLE #
