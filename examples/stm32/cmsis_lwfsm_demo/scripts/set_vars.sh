9efcf407361dfe5b4b00fe116631d7c7f05ab1ce
fred-r-perso <frederic_ruelle@hotmail.fr>
2023-03-26 12:07:04 +0200

    fix: project and environment settings

--- examples/stm32/cmsis_lwfsm_demo/scripts/set_vars.sh
+++ examples/stm32/cmsis_lwfsm_demo/scripts/set_vars.sh
@@ -3,4 +3,9 @@
 export CMSIS_PACK_ROOT="/home/$USER/.arm/Packs"
 echo $CMSIS_PACK_ROOT is available for the child process
 export PATH=$PATH:/home/$USER/.config/Code/User/globalStorage/ms-vscode.vscode-embedded-tools/vcpkg/root/downloads/artifacts/vcpkg-artifacts-cmsis/tools.open.cmsis.pack.ctools/1.5.0/bin
+export PATH=$PATH:/home/$USER/.config/Code/User/globalStorage/ms-vscode.vscode-embedded-tools/vcpkg/root/downloads/artifacts/vcpkg-ce-default/tools.kitware.cmake/3.25.2/bin
+export PATH=$PATH:/home/$USER/.config/Code/User/globalStorage/ms-vscode.vscode-embedded-tools/vcpkg/root/downloads/artifacts/vcpkg-artifacts-community/ninja/1.10.2
+export PATH=$PATH:/home/$USER/.config/Code/User/globalStorage/ms-vscode.vscode-embedded-tools/vcpkg/root/downloads/artifacts/vcpkg-ce-default/compilers.arm.none.eabi.gcc/10.3.1-2021.10/bin
+export ASM="/home/$USER/.config/Code/User/globalStorage/ms-vscode.vscode-embedded-tools/vcpkg/root/downloads/artifacts/vcpkg-ce-default/compilers.arm.none.eabi.gcc/10.3.1-2021.10/bin/arm-none-eabi-as"
+export CC="/home/$USER/.config/Code/User/globalStorage/ms-vscode.vscode-embedded-tools/vcpkg/root/downloads/artifacts/vcpkg-ce-default/compilers.arm.none.eabi.gcc/10.3.1-2021.10/bin/arm-none-eabi-gcc"
 bash
